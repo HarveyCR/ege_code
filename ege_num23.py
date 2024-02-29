e98f730f7d89f73845fb16e3f7c8494c1b6a80a5
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2024-02-29 16:30:14 +0300

    Доработка

--- ege_num23.py
+++ ege_num23.py
@@ -1,16 +1,16 @@
 def calc(begin, end):
-    if begin > end or begin == 15:
+    if begin > end:  # or begin == 15:
         return 0
     if begin == end:
         return 1
     # if begin % 2 == 0:
     #     return calc(begin + 1, end) + calc(2 * begin + 1, end)
     # else:
-    return calc(begin + 1, end)
+    return calc(begin + 1, end) + calc(begin + 5, end)
 
 
 def main():
-    print(calc(1, 25))  # Вызываем функцию калькулятор, от a до b
+    print(calc(5, 34))  # Вызываем функцию калькулятор, от a до b
 
 
 if __name__ == '__main__':
